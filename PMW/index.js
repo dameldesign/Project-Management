c547937427b6fba686382c88faa91f59f43da0eb
dameldesign <98592632+dameldesign@users.noreply.github.com>
2022-06-16 20:28:05 +0100

    Images

--- PMW/index.js
+++ PMW/index.js
@@ -1,5 +1,16 @@
 const mobileMenu = document.querySelector(".menu-toggle")
 const navBar = document.querySelector('.nav-links')
 mobileMenu.addEventListener('click', () =>{
-   navBar.classList.toggle('show')
+   navBar.classList.toggle('show');
+
+   
 })
+
+document.documentElement.addEventListener("mousemove", (e) => {
+   if (e.clientY >= 1.15 * navBar.clientHeight) {
+     navBar.classList.remove("show");
+     mobileMenu.classList.remove("reveal");
+   };   
+   // console.log(e.target)
+   // console.log(navBar.clientHeight)
+});
